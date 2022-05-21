89ca64e27d0ec2d6e329141ccb9db50a6ca994b3
Jaw <tuyweber@gmail.com>
2022-05-21 03:00:12 -0300

    refatorei

--- main.py
+++ main.py
@@ -1,55 +1,79 @@
 import json
+import string
 
-import keyboard
-from keyboard import is_pressed
-
+from typing import Dict
 # 1 - Digitar o nome completo, telefone de contato e e-mail da pessoa
 # 2 - Adicionar elementos a uma lista organizada
 # 3 - Ler a lista item a item
 # 4 - Adicionar os itens da lista aos contatos do google
 
 
-def testar(variavel):
-    if variavel == '':
-        return 0
-    else:
-        return 1
+def remove_punctuation(inputed_string: str) -> str:
+    return inputed_string.translate(str.maketrans('', '', string.punctuation))
 
 
-def repetir(variavel):
-    valor = input(f'{variavel}: ')
-    while testar(valor) == 0:
-        repetir(variavel)
-    return valor
+def adicionar_lista(nome: str,
+                    telefone: str,
+                    email: str) -> Dict[str, str]:
 
-
-def adicionarlista():
-    nome = repetir("Nome")
-    telefone = repetir("Telefone")
-    email = repetir('E-mail')
     dicionario = {
-        'nome': f'{nome}',
-        'telefone': f'{telefone}',
-        'email': f'{email}'
+        "Nome": nome,
+        "Telefone": remove_punctuation(telefone.replace(" ", "")),
+        "Email": email
     }
+
     return dicionario
 
 
-def salvar_json(arquivo, dicionario):
-    with open(arquivo, 'r+') as json_file:
+def salvar_json(dicionario: Dict[str, str]) -> None:
+    with open("contatos.json", "r+") as json_file:
+        lines = json_file.read()
+        if not lines:
+            json_file.write("[]")
+
+    with open("contatos.json", 'r+') as json_file:
         data = json.load(json_file)
         data.append(dicionario)
-    with open(arquivo, 'w') as json_file:
+
+    with open("contatos.json", 'w') as json_file:
         json.dump(data, json_file, indent=4, separators=(',', ': '))
+
+    print("Tudo certo, seu arquivo foi escrito")
 
 
 def loop():
     while True:
-        decidir = input('Para parar digite "0", para continuar, precione qualquer outra tecla. ')
+        decidir = input('Para parar digite "0", para continuar, precione qualquer outra tecla.')
         if decidir == "0":
             break
-        dicionario_add = adicionarlista()
-        salvar_json('contatos.json', dicionario_add)
+
+        nome = input("Digite um nome para o contato: ")
+
+        while not nome:
+            nome = input("Digite um nome para o contato: ")
+
+        telefone = input("Digite um telefone: ")
+
+        while not telefone.isdigit():
+            print("O valor digitado nao era um numero")
+            telefone = input("Digite um telefone: ")
+
+        while not telefone:
+            telefone = input("Digite um telefone: ")
+
+        email = input("Digite um email para o contato: ")
+
+        while not email:
+            email = input("Digite um email para o contato: ")
+
+        dicionario_add = adicionar_lista(
+            nome=nome,
+            email=email,
+            telefone=telefone
+        )
+
+        salvar_json(dicionario_add)
 
 
-loop()
+if __name__ == '__main__':
+    loop()
